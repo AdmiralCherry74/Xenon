41c5adb0ceb25dd799222186604b334b38ef91ff
AdmiralCherry74 <jaznazthesad@gmail.com>
2025-08-31 13:00:17 -0400

    dirt n shit

---
+++ Content/Items/Placeable/Tile/RoughGrass.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Xenon.Content.Items.Placeable.Tile
+{
+	public class RoughGrass : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.RoughGrass>());
+		}
+	}
+}
---
+++ Content/Tiles/RoughGrass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Xenon.Content.Tiles;
+
+public class RoughGrass : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = true;
+        Main.tileMergeDirt[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        AddMapEntry(new Color(44, 106, 55));
+        HitSound = SoundID.Dig;
+        DustType = DustID.JungleGrass;
+    }
+
+    public override void NumDust(int i, int j, bool fail, ref int num)
+    {
+        num = fail ? 1 : 3;
+    }
+}
