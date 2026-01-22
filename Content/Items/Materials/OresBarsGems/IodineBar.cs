73cb183eb68df5f775c1699806fc297f6e39a20e
blahblahbal <jabawakijadehawk@gmail.com>
2026-01-22 08:07:26 -0800

    localization foxes

--- Content/Items/Materials/OresBarsGems/IodineBar.cs
+++ Content/Items/Materials/OresBarsGems/IodineBar.cs
@@ -1,11 +1,7 @@
-using Humanizer;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Xenon.Content.Items.Placeable.Blocks.Natural.OresAndGems;
-using Xenon.Content.Rarities;
-using Xenon.Content.Tiles.Natural.Ores;
-using Xenon.Content.Tiles.Natural.Other;
 
 namespace Xenon.Content.Items.Materials.OresBarsGems;
 
