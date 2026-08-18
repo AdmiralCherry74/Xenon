e83a1102d5fee66ba9b729703fbbaceb90986cba
EmeraldmanJoe <133449862+EmeraldmanJoe@users.noreply.github.com>
2026-08-18 00:09:41 -0600

    Thorn Storm Proj

---
+++ Content/Projectiles/Magic/SpellBookProj/ThornStormProj.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Xenon.Content.Projectiles.Magic.SpellBookProj
+{
+	// Thorn Projectiles, sampled off of the example arrow.
+	public class ThornStormProj : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+		}
+
+		public override void SetDefaults() {
+			Projectile.width = 10; // The width of projectile hitbox
+			Projectile.height = 10; // The height of projectile hitbox
+
+            Projectile.penetrate = 4;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.timeLeft = 1800;
+		}
+
+		public override void AI() {
+            
+			// Apply gravity after a quarter of a second
+			Projectile.ai[0] += 1f;
+			if (Projectile.ai[0] >= 15f) {
+				Projectile.ai[0] = 15f;
+				Projectile.velocity.Y += 0.1f;
+			}
+
+			// The projectile is rotated to face the direction of travel
+			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+			// Cap downward velocity
+			if (Projectile.velocity.Y > 16f) {
+				Projectile.velocity.Y = 16f;
+			}
+		}
+
+		public override void OnKill(int timeLeft) {
+			SoundEngine.PlaySound(SoundID.Dig, Projectile.position); // Plays the basic sound most projectiles make when hitting blocks.
+			for (int i = 0; i < 5; i++) // Creates a splash of dust around the position the projectile dies.
+			{
+				Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Dirt);
+				dust.noGravity = true;
+				dust.velocity *= 1.5f;
+				dust.scale *= 0.9f;
+			}
+		}
+	}
+}
